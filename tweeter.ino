048a999659aef4a7e5bf2d2de60d6500f8ab9c32
AnakinSpain <anakin@atorcha.es>
2019-04-15 18:30:25 +0200

    Delete tweeter.ino

--- tweeter.ino
+++
@@ -1,8 +0,0 @@
-void tweeter(){
-  if (tweet_enviado == false){
-  Serial.println("Envía tweet"); 
-  Blynk.tweet("Estoy online " + String(hour()) + ":" + String(minute()) + ":" + String(second()) + " " + String(day()) +"/" + String(month()) + "/" + String(year()) + " www.atorcha.es");
-  tweet_enviado = true;
-  }
-}
-
